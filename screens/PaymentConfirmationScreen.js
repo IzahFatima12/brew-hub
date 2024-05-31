d98ddd7db75a3ec1565d9ec16554c94f900d02bc
Izah Fatima <sp22-bcs-002@cuilahore.edu.pk>
2024-05-31 12:48:35 -0700

    .

--- screens/PaymentConfirmationScreen.js
+++ screens/PaymentConfirmationScreen.js
@@ -8,7 +8,8 @@ const PaymentConfirmationScreen = () => {
       <Center>
         <Text color={COLORS.Gingerbread} fontSize={24} bold paddingTop={300}>
           Your order has been confirmed!
-          Thankyou for trusting us. We'll make sure you return to order our warm coffeess filled with love.
+          Thankyou for trusting us.
+          We'll make sure you return to order our warm coffeess filled with love.
         </Text>
       </Center>
     </Box>
